dbe6e0cf349b9957e84b05c6a9cd87706a125f8a
Andres <aparrag@cifpfbmoll.eu>
2021-02-20 17:27:06 +0100

    añadido metodo inicialitzarPartida

---
+++ com/jaume/penjat/Tauler.java
@@ -0,0 +1,13 @@
+package com.jaume.penjat;
+
+public class Tauler {
+    private int intents;
+    private char[] paraulaSecreta;
+    private char[] paraulaEndevinada;
+
+    public void inicialitzarPartida(String par, int intents) {
+        this.intents = intents;
+        paraulaSecreta = par.toCharArray();
+        paraulaEndevinada = new char[par.length()];
+    }
+}
